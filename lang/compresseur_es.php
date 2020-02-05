c4c80b4a52d05654c6f6d0b90e3dc9151f8f874e
dani <dani@belvil.net>
2020-02-05 10:16:10 +0100

    [Salvatore] [source:lang/ compresseur] Export depuis https://trad.spip.net de la langue es

--- lang/compresseur_es.php
+++ lang/compresseur_es.php
@@ -9,12 +9,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// E
+	'explication_url_statique_ressources' => 'Una URL que permite servir los archivos estáticos  (imágenes, JS…) sin cookies. Generalmente un sub-dominio alternativo qui apunta al sitio.',
+
 	// I
 	'info_compresseur_titre' => 'Optimizaciones y compresión',
 	'info_question_activer_compactage_css' => 'CSS',
 	'info_question_activer_compactage_js' => 'Scripts',
 	'item_compresseur_css' => 'Activar la compresión de hojas de estilo (CSS)',
 	'item_compresseur_js' => 'Activar la compresión de los scripts (javascript)',
+
+	// L
+	'label_url_statique_ressources' => 'URL recursos',
 
 	// T
 	'texte_compacter_avertissement' => 'Atención a no activar estas opciones durante el desarrollo de tu sitio: los elementos compactados pierden toda legibilidad.',
