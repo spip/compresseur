6e309f4735b2cb8655e1c9b9d6896d68fd3d39ce
Matthieu Marcillaud <marcimat@rezo.net>
2021-12-08 14:48:16 +0100

    Report des chaines de langue depuis master (salvatore)

--- lang/compresseur_it.php
+++ lang/compresseur_it.php
@@ -9,12 +9,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// E
+	'explication_url_statique_ressources' => 'Un URL che consente di offrire file statici (immagini, JS, ecc...) senza cookie. Di solito un sottodominio alternativo che punta anche al sito.',
+
 	// I
 	'info_compresseur_titre' => 'Ottimizzazione e compressione',
 	'info_question_activer_compactage_css' => 'CSS',
 	'info_question_activer_compactage_js' => 'Scripts',
 	'item_compresseur_css' => 'Attiva la compressione dei foglio di stile (CSS)',
 	'item_compresseur_js' => 'Attivare la compressione degli script (javascript)',
+
+	// L
+	'label_url_statique_ressources' => 'URL delle risorse',
 
 	// T
 	'texte_compacter_avertissement' => 'Fare attenzione a non attivare queste opzioni durante la fase di sviluppo del sito: gli elementi compressi sono completamente illeggibili.',
