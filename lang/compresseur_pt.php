5ed83896dabfa632e25888ed19bfa3a3e63e131f
Jorge Paulo Malveira Pimentel Dos Santos <jpmpds@yahoo.fr>
2022-01-04 01:27:09 +0100

    [Salvatore] [source:lang/ compresseur] Export depuis https://trad.spip.net de la langue pt [Salvatore] [source:lang/ compresseur] Mise a jour du bilan depuis https://trad.spip.net

--- lang/compresseur_pt.php
+++ lang/compresseur_pt.php
@@ -9,12 +9,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// E
+	'explication_url_statique_ressources' => 'Um URL que permite que arquivos estáticos (imagens, JS, etc.) sejam exibidos sem cookies. Normalmente, um subdomínio alternativo que também aponta para o sítio.',
+
 	// I
 	'info_compresseur_titre' => 'Optimização e compressão',
 	'info_question_activer_compactage_css' => 'CSS',
 	'info_question_activer_compactage_js' => 'Scripts',
 	'item_compresseur_css' => 'Activar a compressão das folhas de estilo (CSS)',
 	'item_compresseur_js' => 'Activar a compressão de scripts (javascript)',
+
+	// L
+	'label_url_statique_ressources' => 'URL do recurso', # RELIRE
 
 	// T
 	'texte_compacter_avertissement' => 'Tenha cuidado para não activar estas opções durante o desenvolvimento do seu sítio, pois os elementos comprimidos são difíceis de ler e depurar.',
